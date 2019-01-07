c3940b76c11303c1793eb47e1599358437d13246
xuyouchang <xuyouchang@treefinance.com.cn>
2019-01-07 15:04:11 +0800

    add pipeline shell

--- add_pipeline.sh
+++ add_pipeline.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-ansible-playbook -i hosts ../../logstash/playbook/add_pipeline.yml -e "pipeline_path=test.conf"
+if [ $# -ne 3 ]
+then 
+    echo "please check the Useage: bash add_pipeline.sh hostname pipeline_conf_name pipeline_yml"
+    exit
+fi
+echo $1 $2 $3
+ansible-playbook -i production/hosts site.yml --limit $1 -e "pipeline_conf_name=$2" -e "pipeline_yml=$3" -e "group_server=logstash" -e "action=add_pipeline" -e "rolename=logstash"
