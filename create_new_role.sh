bf25f86587c72f5ff0a93d56d7c8bd2503476386
xuyouchang <xuyouchang@treefinance.com.cn>
2019-01-07 10:48:45 +0800

    add create role shell

---
+++ create_new_role.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ $# ne 1 ]
+then
+  echo please check the Usesage: bash create_new_role.sh "new_role_name"
+  exit
+if
+
+mkdir -p roles/$1/{defaults,file,handlers,meta,tasks,template,vars}
