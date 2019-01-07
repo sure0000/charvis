35a28929f908881a92eb95c196895122da7ec249
xuyouchang <xuyouchang@treefinance.com.cn>
2019-01-07 15:23:27 +0800

    add prometheus

--- create_new_role.sh
+++ create_new_role.sh
@@ -3,6 +3,5 @@ if [ $# -ne 1 ]
 then
   echo please check the Usesage: bash create_new_role.sh "new_role_name"
   exit
-if
-
+fi
 mkdir -p roles/$1/{defaults,files,handlers,meta,tasks,template,vars}
